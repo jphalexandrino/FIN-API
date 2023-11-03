0a2ba2c80b9688959c931e64aa9a6b75756d001d
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-11-03 13:33:30 +0000

    Criado o listamento de status

--- src/index.js
+++ src/index.js
@@ -28,5 +28,13 @@ app.post("/accont", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
+
 app.listen(3333);
 //  http://localhost:3333
