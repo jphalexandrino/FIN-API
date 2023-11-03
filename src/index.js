35530e0a3501475911c1e906d85403569f9bd2ec
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-11-03 13:46:45 +0000

    Validando a conta

--- src/index.js
+++ src/index.js
@@ -28,10 +28,14 @@ app.post("/accont", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(404).json({ error: "Customer not found!" });
+  }
 
   return response.json(customer.statement);
 });
