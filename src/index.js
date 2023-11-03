f02c6bae6d8ec5838673111529faf6163df4e052
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-11-03 22:58:53 +0000

    Criado o sistema de saque

--- src/index.js
+++ src/index.js
@@ -22,6 +22,17 @@ function verifyIfExistsAccontCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type == "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
+
 app.post("/accont", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -62,6 +73,27 @@ app.post("/deposit", verifyIfExistsAccontCPF, (request, response) => {
   };
 
   customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccontCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
   return response.status(201).send();
 });
 
